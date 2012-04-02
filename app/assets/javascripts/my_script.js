608426a6662a69eacab1ac8979693718295b6fd8
Alexis Chuck <qenoah@gmail.com>
2012-04-02 09:06:39 -0700

    sqlite thing fixed (maybe)

--- app/assets/javascripts/my_script.js
+++ app/assets/javascripts/my_script.js
@@ -31,8 +31,33 @@ function init() {
        			location: myLatLong,
        			radius: 500,
        			types: ['restaruant']
-       		}
+       		};
+       		infowindow = new google.maps.InfoWindow();
+	     		var service = new google.maps.places.PlacesService(map);
+   		      service.search(request, callback);
+        
 		});
 	} /* close if */
 
 }
+
+ function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+          }
+        }
+      }
+
+      function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent(place.name);
+          infowindow.open(map, this);
+        });
+      }
