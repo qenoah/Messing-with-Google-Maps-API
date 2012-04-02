d971ec78a3a3284b89ea4a2381fbfb12c7bbc7c8
Alexis Chuck <qenoah@gmail.com>
2012-04-02 07:00:27 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThreeApi::Application.config.secret_token = '281b22a6b8df8f4ea04247d4221af8c1c89a516db8ebe609d2cebe8ec51438af124909fd7254b1597c26e977e5696c83bce364429116b130d3d967b39c059094'
